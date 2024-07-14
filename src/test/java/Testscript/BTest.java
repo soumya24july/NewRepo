0b8381e47c0b28a279538d85e4bdf6a9108d2a8b
soumya24july <soumya24july@gmail.com>
2024-07-14 20:39:35 +0530

    commit

---
+++ src/test/java/Testscript/BTest.java
@@ -0,0 +1,17 @@
+package Testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BTest {
+	
+	
+	@Test
+	public void m2()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.cricbuzz.com");
+	}
+
+}
